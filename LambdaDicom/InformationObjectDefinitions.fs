4b65966d75ec0347ea2e151270f1fc3aa8a282d2
Derek Lane <dglane001@hotmail.com>
2018-08-09 12:10:48 -0500

    changed modules and IODs to be classes (in prep for TypeProvider generation)

---
+++ LambdaDicom/InformationObjectDefinitions.fs
@@ -0,0 +1,19 @@
+﻿namespace LambdaDicom
+
+type InformationObjectDefinitionAttribute() = inherit System.Attribute()
+
+[<InformationObjectDefinition>]
+type ComputedTomographyImage(SOPCommon:SOPCommonModule, 
+                             Patient:PatientModule, 
+                             GeneralImage:GeneralImageModule) =
+    member this.SOPCommon = SOPCommon
+    member this.Patient = Patient
+    member this.GeneralImage = GeneralImage
+    member this.asAttributes = 
+        SOPCommon.asAttributes
+        |> List.append Patient.asAttributes
+        |> List.append GeneralImage.asAttributes
+    static member fromAttributes attributes =
+        ComputedTomographyImage(SOPCommonModule.fromAttributes attributes,
+                                PatientModule.fromAttributes attributes,
+                                GeneralImageModule.fromAttributes attributes)
---
+++ LambdaDicom/Modules.fs
@@ -0,0 +1,33 @@
+﻿namespace LambdaDicom
+
+open System
+
+type ModuleAttribute() = inherit System.Attribute()
+
+[<Module>]
+type SOPCommonModule(SOPClassUID:UniqueIdentifier, 
+                     SOPInstanceUID:UniqueIdentifier) =
+    member this.SOPClassUID = SOPClassUID
+    member this.SOPInstanceUID = SOPInstanceUID
+    member this.asAttributes : LambdaDicom.Attribute list =
+        [ {Tag={Group=0x0000u; Element=0x0000u}; Values=UI [| this.SOPClassUID |]};
+          {Tag={Group=0x0000u; Element=0x0000u}; Values=UI [| this.SOPInstanceUID |]} ]
+    static member fromAttributes attributes =
+        SOPCommonModule(attributes |> LambdaDicom.Attribute.findHeadValue<UniqueIdentifier> {Group=0x0000u; Element=0x0000u},
+                        attributes |> LambdaDicom.Attribute.findHeadValue<UniqueIdentifier> {Group=0x0000u; Element=0x0000u})
+
+[<Module>]
+type PatientModule(PatientId:string) =
+    member this.PatientId = PatientId
+    member this.asAttributes : LambdaDicom.Attribute list =
+        [ {Tag={Group=0x0000u; Element=0x0000u}; Values=UI [| this.PatientId |]}; ]
+    static member fromAttributes attributes =
+        PatientModule(attributes |> LambdaDicom.Attribute.findHeadValue<UniqueIdentifier> {Group=0x0000u; Element=0x0000u})
+
+[<Module>]
+type GeneralImageModule(AcquisitionDateTime:DateTime) =
+    member this.AcquisitionDateTime = AcquisitionDateTime
+    member this.asAttributes : LambdaDicom.Attribute list =
+        [ {Tag={Group=0x0000u; Element=0x0000u}; Values=DT [| this.AcquisitionDateTime |]}; ]
+    static member fromAttributes attributes =
+        GeneralImageModule(attributes |> LambdaDicom.Attribute.findHeadValue<DateTime> {Group=0x0000u; Element=0x0000u})
