272d564c26d0b7641c1005d95eff08911ac09b22
Derek Lane <dglane001@hotmail.com>
2018-09-24 12:09:45 -0500

    example of a blob storage WebAPI

---
+++ BlobStorageApi/Controllers/BlobController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobStorageApi.Controllers
+{
+    [Route("/api/blob")]
+    public class BlobController : ControllerBase
+    {
+        static Dictionary<int, byte[]> _blobs = new Dictionary<int, byte[]>();
+
+        [HttpGet]
+        [Route("{blobId}")]
+        public IActionResult GetBinaryDataManual(int blobId)
+        {
+            byte[] blobValue;
+            if (_blobs.TryGetValue(blobId, out blobValue))
+            {
+                return File(blobValue, "application/octet-stream");
+            }
+
+            return NotFound();
+         }
+
+        [HttpPost]
+        [Route("PostBinaryDataManual")]
+        public async Task<int> PostBinaryDataManual()
+        {
+            using (var ms = new MemoryStream(2048))
+            {
+                await Request.Body.CopyToAsync(ms);
+                byte[] blobValue = ms.ToArray();
+                int id = _blobs.Keys.OrderBy(ky => ky).Last() + 1;
+                _blobs.Add(id, blobValue);
+                return id;
+            }
+        }
+    }
+}
